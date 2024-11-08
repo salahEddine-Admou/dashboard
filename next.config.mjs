d6fba685475e0a4cef1dae7a856a7616392a63be
salah-eddine <salma.1994.199921@gmail.com>
2024-11-08 16:00:17 +0100

    sc

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com", "swe9a.com"],  // Add your domain here
   },
 };
 
 export default nextConfig;
+
